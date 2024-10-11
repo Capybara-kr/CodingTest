a367193065cb2c09920848284cff898827d70bed
Capybara-kr <rlfdudrms02@gmail.com>
2024-10-11 22:51:52 +0900

    [level 0] Title: 배열의 유사도, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/README.md
+++ 프로그래머스/0/120903. 배열의 유사도/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열의 유사도 - 120903 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120903) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120903?language=cpp) 
 
 ### 성능 요약
 
-메모리: 4.15 MB, 시간: 0.01 ms
+메모리: 4.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 11일 22:48:46
+2024년 10월 11일 22:51:51
 
 ### 문제 설명
 
--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.cpp
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.cpp
@@ -1,15 +1,15 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int solution(vector<string> s1, vector<string> s2) {
     int answer = 0;
     
-    for (auto iterS1 : s1) {
-        for (auto iterS2: s2) {
-            if (iterS1 == iterS2) answer++;
-        }
+    for (auto iter : s1) {
+        if (find(s2.begin(), s2.end(), iter) != s2.end()) 
+            answer++;
     }
     
     return answer;

996905dddec7d2af7832f6cf55ba20d5449b5cdc
Capybara-kr <rlfdudrms02@gmail.com>
2024-10-11 23:10:59 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.22 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/120809. 배열 두배 만들기/README.md
+++ 프로그래머스/0/120809. 배열 두배 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열 두배 만들기 - 120809 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120809) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120809?language=cpp) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 0.33 ms
+메모리: 4.14 MB, 시간: 0.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 11일 23:08:56
+2024년 10월 11일 23:10:58
 
 ### 문제 설명
 
--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.cpp
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.cpp
@@ -1,13 +1,17 @@
 #include <string>
 #include <vector>
-#include <iostream>
+
 using namespace std;
 
 vector<int> solution(vector<int> numbers) {
     vector<int> answer;
     
+    // for (auto iter : numbers) {
+    //     answer.push_back(iter * 2);
+    // }
+    
     for (auto iter : numbers) {
-        answer.push_back(iter * 2);
+        answer.push_back(iter << 1);
     }
     
     return answer;

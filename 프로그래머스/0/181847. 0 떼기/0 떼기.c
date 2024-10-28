f2db49a88558f37c4dd831dba6666fa235c5af26
Capybara-kr <rlfdudrms02@gmail.com>
2024-10-28 18:17:24 +0900

    [level 0] Title: 0 떼기, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.c
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.c
@@ -5,16 +5,20 @@
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 char* solution(const char* n_str) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
-    size_t n_str_len = strlen(n_str);
-    char* answer = (char*)malloc(n_str);
-    int idx = 0;
+//  size_t n_str_len = strlen(n_str);
+    char* answer = n_str - 1;
+//     int idx = 0;
     
-    for ( int i = 0; i < n_str_len; i++ ) {
-        if (n_str[0] == '0' && i == 0) {
-            while (n_str[++i] == '0');
-        }
-        answer[idx++] = n_str[i];
-    }
+//     for ( int i = 0; i < n_str_len; i++ ) {
+//         if (n_str[0] == '0' && i == 0) {
+//             while (n_str[++i] == '0');
+//         }
+//         answer[idx++] = n_str[i];
+//     }
 
+    while (*++answer) {
+        if (*answer != '0') break;
+    }
+    
     return answer;
 }
--- 프로그래머스/0/181847. 0 떼기/README.md
+++ 프로그래머스/0/181847. 0 떼기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 0 떼기 - 181847 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181847) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181847?language=c) 
 
 ### 성능 요약
 
-메모리: 4.14 MB, 시간: 0.02 ms
+메모리: 3.67 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 28일 18:14:31
+2024년 10월 28일 18:17:21
 
 ### 문제 설명
 

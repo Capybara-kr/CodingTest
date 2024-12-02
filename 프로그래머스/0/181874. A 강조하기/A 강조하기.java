ce1f4324ded322641e0d623f88ff51753e274c4b
Capybara-kr <rlfdudrms02@gmail.com>
2024-12-02 16:28:47 +0900

    [level 0] Title: A 강조하기, Time: 22.89 ms, Memory: 83.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        
+        for (int i = 0; i < myString.length(); i++) {
+            char currentChar = myString.charAt(i);
+            if (currentChar == 'a' || currentChar == 'A') {
+                answer += Character.toUpperCase(currentChar);
+            } else {
+                answer += Character.isUpperCase(currentChar) ? Character.toLowerCase(currentChar) : currentChar;
+            }
+        }
+        
+        return answer;
+    }
+}

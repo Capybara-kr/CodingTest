1964e06e25f249331256c33dd37021a62fcfd161
Capybara-kr <rlfdudrms02@gmail.com>
2024-12-12 16:27:17 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.05 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        String num = String.valueOf(n);
+        
+        for (int i = 0; i < num.length(); i++) {
+            answer += Character.getNumericValue(num.charAt(i));
+        }
+        
+        return answer;
+    }
+}
